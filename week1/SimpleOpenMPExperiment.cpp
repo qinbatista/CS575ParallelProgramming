19bdffdfe6d0bf0440992356d9937f3f1b30ebed
qin yu peng <qin.batista@outlook.com>
2022-03-30 18:32:15 -0700

    ss

--- week1/SimpleOpenMPExperiment.cpp
+++ week1/SimpleOpenMPExperiment.cpp
@@ -20,8 +20,8 @@ int
 main( )
 {
 #ifndef _OPENMP
-        fprintf( stderr, "OpenMP is not supported here -- sorry.\n" );
-        return 1;
+		fprintf( stderr, "OpenMP is not supported here -- sorry.\n" );
+		return 1;
 #endif
 
 	// inialize the arrays:
@@ -30,29 +30,30 @@ main( )
 		A[i] = 1.;
 		B[i] = 2.;
 	}
-    for( int thread_idx = 1; thread_idx <= NUMT; thread_idx++ )
-    {
-        omp_set_num_threads( thread_idx );
-        // fprintf( stderr, "Using %d threads\n", thread_idx );
-        double maxMegaMults = 0.;
-        for( int t = 0; t < NUMTRIES; t++ )
-        {
-                double time0 = omp_get_wtime( );
-
-                #pragma omp parallel for
-                for( int i = 0; i < SIZE; i++ )
-                {
-                        C[i] = A[i] * B[i];
-                }
-
-                double time1 = omp_get_wtime( );
-                double megaMults = (double)SIZE/(time1-time0)/1000000.;
-                if( megaMults > maxMegaMults )
-                        maxMegaMults = megaMults;
-        }
-        printf( "Using %d threads:Peak Performance = %8.2lf MegaMults/Sec\n", thread_idx, maxMegaMults );
-    }
-    // note: %lf stands for "long float", which is how printf prints a "double"
-    //        %d stands for "decimal integer", not "double"
-    return 0;
+	for( int thread_idx = 1; thread_idx <= NUMT; thread_idx++ )
+	{
+		omp_set_num_threads( thread_idx );
+		// fprintf( stderr, "Using %d threads\n", thread_idx );
+		double maxMegaMults = 0.;
+		for( int trailer = 1; trailer < NUMTRIES+1; trailer++)
+		{
+			for( int t = 0; t < trailer; t++ )
+			{
+				double time0 = omp_get_wtime( );
+				#pragma omp parallel for
+				for( int i = 0; i < SIZE; i++ )
+				{
+						C[i] = A[i] * B[i];
+				}
+				double time1 = omp_get_wtime( );
+				double megaMults = (double)SIZE/(time1-time0)/1000000.;
+				if( megaMults > maxMegaMults )
+						maxMegaMults = megaMults;
+			}
+			printf("%d	threads	%d	trailer:Peak Performance =	%8.2lf	MegaMults/Sec\n", thread_idx,trailer, maxMegaMults );
+		}
+	}
+	// note: %lf stands for "long float", which is how printf prints a "double"
+	//        %d stands for "decimal integer", not "double"
+	return 0;
 }
