871e8b328a90cd5924fa34f14a1ff336e3eefccf
Yupeng Qin <qinyu@rabbit.engr.oregonstate.edu>
2022-04-04 21:59:21 -0700

    base

--- week2/run_script.py
+++ week2/run_script.py
@@ -1,10 +1,15 @@
 import os
 import subprocess
-
+from sys import platform
 def StartTest():
     for thread_number in [1,2,4,8,12,16,20,24,32]:
         for trail_number in [1,10,100,1000,10000,100000,500000,1000000]:
-            os.system(f"g++ -Xpreprocessor -fopenmp montecarlo.cpp -lomp -o montecarlo -DNUMT={thread_number} -DNUMTRIALS={trail_number}")
+
+            if platform == "linux" or platform == "linux2":
+                os.system(f"g++  -fopenmp montecarlo.cpp -o montecarlo -DNUMT={thread_number} -DNUMTRIALS={trail_number}")
+            elif platform == "darwin":
+                os.system(f"g++ -Xpreprocessor -lomp -fopenmp montecarlo.cpp  -o montecarlo -DNUMT={thread_number} -DNUMTRIALS={trail_number}")
+
             p = subprocess.Popen(f"./montecarlo",  stderr= open("./proj1.csv",'a'), universal_newlines=True, shell=True)
             p.wait()
 if __name__ == '__main__':
