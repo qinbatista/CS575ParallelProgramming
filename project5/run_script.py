ac297bb5537b22801d1cdf109efb335e29c3398f
MacStudio <qin>
2022-05-04 18:04:11 -0700

    ss

--- project5/run_script.py
+++ project5/run_script.py
@@ -5,7 +5,7 @@ def StartTest():
     for block_size in [1,2,4,8,16,32,64,128.256,512]:
         for trail_number in [1024, 4096, 16384, 65536, 262144, 1048576, 2097152, 4194304]:
             if platform == "linux" or platform == "linux2":
-                os.system(f"/usr/local/apps/cuda/cuda-10.1/bin/nvcc -o montecarlo montecarlo.cu -DNUMTRIALS={trail_number} -BLOCKSIZE={block_size}")
+                os.system(f"/usr/local/apps/cuda/cuda-10.1/bin/nvcc -o montecarlo montecarlo.cu -DNUMTRIALS={trail_number} -DBLOCKSIZE={block_size}")
             p = subprocess.Popen(f"./montecarlo",  stderr= open("./montecarlo.csv",'a'), universal_newlines=True, shell=True)
             p.wait()
 if __name__ == '__main__':
