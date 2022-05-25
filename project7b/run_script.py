f8ba0f0748004deb2aae5b02b9d9e666a4158230
MacStudio <qin>
2022-05-24 21:45:50 -0700

    ss

--- project7b/run_script.py
+++ project7b/run_script.py
@@ -3,8 +3,10 @@ import subprocess
 from sys import platform
 def StartTest():
     for thread_size in [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32]:
-        os.system(f"mpic++ main.cpp -o main -lm")
-        p = subprocess.Popen("mpiexec -mca btl self,tcp -np {thread_size} main",  stderr= open("./montecarlo.csv",'a'), universal_newlines=True, shell=True)
+        # os.system(f"mpic++ main.cpp -o main -lm")
+        pa = subprocess.Popen(f"mpic++ main.cpp -o main -lm",  stderr= open("./error.csv",'a'), universal_newlines=True, shell=True)
+        pa.wait()
+        p = subprocess.Popen(f"mpiexec -mca btl self,tcp -np {thread_size} main",  stderr= open("./log.csv",'a'), universal_newlines=True, shell=True)
         p.wait()
 if __name__ == '__main__':
     StartTest()
